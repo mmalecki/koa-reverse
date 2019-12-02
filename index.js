ccbf8b2b616b4d8ae275904c3a8e6039497feaf8
Maciej Małecki <me@mmalecki.com>
2019-12-02 22:22:11 +0100

    fix: only pass pathname to routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const url = require('url')
+
 module.exports = function (routes) {
   return async function (ctx, next) {
-    const match = routes.match(ctx.method, ctx.url)
+    const parsed = url.parse(ctx.url)
+    const match = routes.match(ctx.method, parsed.pathname)
     if (!match) return ctx.throw(404)
     const route = match.controller[match.name]
     ctx.request.body = Promise.resolve(ctx.request.body)
